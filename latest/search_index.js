f71653478c60c6590f1353495a14677e7b8d23ab
autodocs <autodocs>
2018-03-12 22:07:19 +0000

    build based on abff326f

--- latest/search_index.js
+++ latest/search_index.js
@@ -81,11 +81,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "usage/spaces.html#Classical-orthogonal-polynomial-spaces-1",
+    "page": "Spaces",
+    "title": "Classical orthogonal polynomial spaces",
+    "category": "section",
+    "text": "Chebyshev, Ultraspherical, Jacobi, Hermite, and Laguerre are spaces corresponding to expansion in classical orthogonal polynomials.Note that we always use the classical normalization: the basis are _not_ orthonormal. This is because this normalization leads to rational recurrence relationships, which are more efficient than their normalized counterparts. See the Digital Library of Mathematical Functions for more information."
+},
+
+{
     "location": "usage/spaces.html#Chebyshev-space-1",
     "page": "Spaces",
     "title": "Chebyshev space",
     "category": "section",
-    "text": "The default space in ApproxFun is Chebyshev, which represents expansions in Chebyshev polynomials:f(x) = sum_k=0^infty f_k T_k(x)where T_k(x) = cos k rm acos x.   Note that there is an intrinsic link between Chebyshev and CosSpace:  g(theta) = f(cos theta) = sum_k=0^infty f_k cos k thetaIn other words:DocTestSetup = quote\n    using ApproxFun\nendjulia> f=Fun(exp,Chebyshev());\n\njulia> g=Fun(CosSpace(),f.coefficients); # specify the coefficients directly\n\njulia> f(cos(0.1))\n2.70473560723178\n\njulia> g(0.1)\n2.7047356072317794"
+    "text": "The default space in ApproxFun is Chebyshev, which represents expansions in Chebyshev polynomials:f(x) = sum_k=0^infty f_k T_k(x)where T_k(x) = cos k rm acos x, which are orthogonal polynomials with respect to the weight $ {1 \\over \\sqrt{1-x^2} \\qquad\\hbox{for}\\qquad -1 \\leq x \\leq 1. $ Note that there is an intrinsic link between Chebyshev and CosSpace:  g(theta) = f(cos theta) = sum_k=0^infty f_k cos k thetaIn other words:DocTestSetup = quote\n    using ApproxFun\nendjulia> f=Fun(exp,Chebyshev());\n\njulia> g=Fun(CosSpace(),f.coefficients); # specify the coefficients directly\n\njulia> f(cos(0.1))\n2.70473560723178\n\njulia> g(0.1)\n2.7047356072317794"
 },
 
 {
@@ -93,7 +101,15 @@ var documenterSearchIndex = {"docs": [
     "page": "Spaces",
     "title": "Ultraspherical spaces",
     "category": "section",
-    "text": "A key tool for solving differential equations are the ultraspherical spaces, which can be defined by the span of derivatives of Chebyshev polynomials.  Note that Ultraspherical(1) corresponds to the Chebyshev basis of the second kind: U_k(x) = sin (k+1) rm acos x over sin rm acos x.   The relationship with Chebyshev polynomials follows from trigonemetric identities: T_k(x) = k U_k-1(x).  Converting between ultraspherical polynomials (with integer orders) is extremely efficient: it requires O(n) operations, where n is the number of coefficients."
+    "text": "A key tool for solving differential equations are the ultraspherical spaces, encoded as Ultraspherical(λ) for λ ≠ 0, which can be defined by the span of derivatives of Chebyshev polynomials, or alternatively as polynomials orthogonal with respect to the weight $ (1-x^2)^{\\lambda - 1/2} \\qquad\\hbox{for}\\qquad -1 \\leq x \\leq 1. $Note that Ultraspherical(1) corresponds to the Chebyshev basis of the second kind: U_k(x) = sin (k+1) rm acos x over sin rm acos x.   The relationship with Chebyshev polynomials follows from trigonemetric identities: T_k(x) = k U_k-1(x).  Converting between ultraspherical polynomials (with integer orders) is extremely efficient: it requires O(n) operations, where n is the number of coefficients."
+},
+
+{
+    "location": "usage/spaces.html#Jacobi-spaces-1",
+    "page": "Spaces",
+    "title": "Jacobi spaces",
+    "category": "section",
+    "text": "Jacobi(b,a) represents the space spanned by the Jacobi polynomials, which are orthogonal polynomials with respect to the weight $ (1+x)^b(1-x)^a $ using the standard normalization."
 },
 
 {
@@ -109,7 +125,47 @@ var documenterSearchIndex = {"docs": [
     "page": "Spaces",
     "title": "Modifier spaces",
     "category": "section",
-    "text": "Some spaces are built out of other spaces.  A simple example is JacobiWeight(β,α,space) which weights space, which is typically Chebyshev() or Jacobi(b,a),  by a Jacobi weight (1+x)^b*(1-x)^a.@meta  DocTestSetup = nothing"
+    "text": "Some spaces are built out of other spaces:"
+},
+
+{
+    "location": "usage/spaces.html#JacobiWeight-1",
+    "page": "Spaces",
+    "title": "JacobiWeight",
+    "category": "section",
+    "text": "JacobiWeight(β,α,space)  weights space, which is typically Chebyshev() or Jacobi(b,a),  by a Jacobi weight (1+x)^α*(1-x)^β: in other words, if the basis for space is psi_k(x) and the domain is the unit interval -1 .. 1, then the basis for JacobiWeight(β,α,space) is (1+x)^(1-x)^ psi_k(x). If the domain is not the unit interval, then the basis is determined by mapping back to the unit interval: that is, if M(x) is the map dictated by tocanonical(space, x), where the canonical domain is the unit interval, then the basis is (1+M(x))^(1-M(x))^ psi_k(x). For example, if the domain is another interval a .. b, then $ M(x) = {2x-b-a \\over b-a} $ and the basis becomes $ \\left({2 \\over (b-a)}\\right)^{\\alpha+\\eta}  (x-a)^α(b-x)^β \\psi_k(x) $"
+},
+
+{
+    "location": "usage/spaces.html#SumSpace-1",
+    "page": "Spaces",
+    "title": "SumSpace",
+    "category": "section",
+    "text": "SumSpace((space_1,space_2,…,space_n)) represents the direct sum of the spaces, where evaluation is defined by adding up each component. A simple example is the following, showing that the coefficients are stored by interlacing:julia> f = cos(x-0.1)*sqrt(1-x^2) + exp(x);\n\njulia> space(f) # isa SumSpace\n(1-x^2)^0.5[Chebyshev(【-1.0,1.0】)]⊕Chebyshev(【-1.0,1.0】)\n\njulia> a, b = components(f);\n\njulia> a(0.2) # cos(0.2-0.1)*sqrt(1-0.2^2)\n0.9749009987500246\n\njulia> b(0.2) # exp(0.2)\n1.2214027581601699\n\njulia> f(0.2) # a(0.2) + b(0.2)\n2.1963037569101944\n\njulia> norm(f.coefficients[1:2:end] - a.coefficients)\n0.0\n\njulia> norm(f.coefficients[2:2:end] - b.coefficients)\n0.0More complicated examples may interlace the coefficients using a different strategy.Note that Fourier and Laurent are currently implemented as SumSpace, but this may change in the future."
+},
+
+{
+    "location": "usage/spaces.html#PiecewiseSpace-1",
+    "page": "Spaces",
+    "title": "PiecewiseSpace",
+    "category": "section",
+    "text": "PiecewiseSpace((space_1,space_2,…,space_n)) represents the direct sum of the spaces, where evaluation is defined in a piecewise way. A simple example is the following:julia> x = Fun(Domain(-1 .. 0) ∪ Domain( 1 .. 2));\n\njulia> f = exp(x);\n\njulia> a, b = components(f);\n\njulia> f(-0.5) - a(-0.5)\n0.0\n\njulia> f(1.5) - b(1.5)\n0.0\n\njulia> f(0.5) # outside domain components\n0.0\n\njulia> norm(f.coefficients[2:2:end] - b.coefficients)\n0.0\n\njulia> norm(f.coefficients[1:2:end] - a.coefficients)\n0.0More complicated examples may interlace the coefficients using a different strategy."
+},
+
+{
+    "location": "usage/spaces.html#ArraySpace-1",
+    "page": "Spaces",
+    "title": "ArraySpace",
+    "category": "section",
+    "text": "ArraySpace(::Array{<:Space}) represents the direct sum of the spaces, where evaluation is defined in an array-wise manner. A simple example is the following:julia> f = [exp(x); sqrt(1-x^2)*cos(x-0.1)];\n\njulia> space(f)\n2-element ArraySpace:\n Chebyshev(【-1.0,1.0】)             \n (1-x^2)^0.5[Chebyshev(【-1.0,1.0】)]\n\njulia> a, b = components(f);\n\njulia> norm(f(0.5) - [a(0.5); b(0.5)])\n0.0\n\njulia> norm(f.coefficients[1:2:end] - a.coefficients)\n0.0\n\njulia> norm(f.coefficients[2:2:end] - b.coefficients)\n0.0More complicated examples may interlace the coefficients using a different strategy."
+},
+
+{
+    "location": "usage/spaces.html#TensorSpace-1",
+    "page": "Spaces",
+    "title": "TensorSpace",
+    "category": "section",
+    "text": "TensorSpace((space_1,space_2)) represents the tensor product of two spaces. See documentation of TensorSpace for more details on how the coefficients are interlaced. Note that more than two spaces is only partially supported."
 },
 
 {
