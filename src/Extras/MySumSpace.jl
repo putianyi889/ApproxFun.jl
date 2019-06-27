032e33e594872911cd376a63f3c2a1888235170b
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2019-06-27 12:44:20 +0100

    Update MySumSpace.jl

--- src/Extras/MySumSpace.jl
+++ src/Extras/MySumSpace.jl
@@ -1,28 +1,3 @@
-const MySumSpace = Array{<:Space,1}
 
-struct MySumSpace{SV,D,R}<:DirectSumSpace{SV,D,R}
-    spaces::SV
-    MySumSpace{SV,D,R}(dom::Domain) where {SV,D,R} =
-        new(tuple(map(typ->typ(dom),SV.parameters)...))
-    MySumSpace{SV,D,R}(sp::Tuple) where {SV,D,R} = new(sp)
-end
-
-MySumSpace(sp::Tuple) = MySumSpace{typeof(sp),domaintype(first(sp)),
-                                mapreduce(rangetype,promote_type,sp)}(sp)
-
-MySumSpace(A::MySumSpace,B::MySumSpace) = MySumSpace(tuple(A.spaces...,B.spaces...))
-MySumSpace(A::Space,B::MySumSpace) = MySumSpace(tuple(A,B.spaces...))
-MySumSpace(A::MySumSpace,B::Space) = MySumSpace(tuple(A.spaces...,B))
-MySumSpace(A::Space...) = MySumSpace(A)
-MySumSpace(sp::AbstractArray) = MySumSpace(tuple(sp...))
-
-canonicalspace(A::MySumSpace) = MySumSpace(sort(collect(A.spaces)))
-
-MySumSpace(A::ConstantSpace{AnyDomain}, B::ConstantSpace{AnyDomain}) = error("Should not happen")
-MySumSpace(A::SumSpace, B::ConstantSpace{AnyDomain}) = MySumSpace(A, setdomain(B, domain(A)))
-MySumSpace(B::ConstantSpace{AnyDomain}, A::SumSpace) = MySumSpace(setdomain(B, domain(A)), A)
-MySumSpace(A::Space, B::ConstantSpace{AnyDomain}) = MySumSpace(A, setdomain(B, domain(A)))
-MySumSpace(B::ConstantSpace{AnyDomain}, A::Space) = MySumSpace(setdomain(B, domain(A)), A)
-
-setdomain(A::MySumSpace,d::Domain) = MySumSpace(map(sp->setdomain(sp,d),A.spaces))
+evaluate(f::AbstractArray{<:AbstractArray,1}, S::Array{<:Space,1}, x) = sum(evaluate.(f,S,x))
 
