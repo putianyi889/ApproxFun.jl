fb13cd5199264abed154e6fef3cdc39724431fd2
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2019-06-28 00:18:40 +0100

    Update MySumSpace.jl

--- src/Extras/MySumSpace.jl
+++ src/Extras/MySumSpace.jl
@@ -3,5 +3,5 @@ evaluate(f::AbstractArray{<:AbstractArray,1}, S::Array{<:Space,1}, x) = sum(eval
 
 function Conversion(S1::Array{<:Space,1}, S2::Array{<:Space,1})
     @assert size(S1) == size(S2)
-    [m==n?Conversion(S1[m],S2[n]):ZeroOperator(S1[m],S2[n]) for n in 1:length(S2), m in 1:length(S1)]
+    [m==n ? Conversion(S1[m],S2[n]) : ZeroOperator(S1[m],S2[n]) for n in 1:length(S2), m in 1:length(S1)]
 end
