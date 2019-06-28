67d2074d0063e2c2e8982d2ff0b73ea3050dfe7a
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2019-06-28 15:01:31 +0100

    Update MySumSpace.jl

--- src/Extras/MySumSpace.jl
+++ src/Extras/MySumSpace.jl
@@ -3,13 +3,16 @@ export MySumSpace,MatrixOperator
 struct MySumSpace{D,R} <: Space{D,R}
     spaces::Array{<:Space{D,R},1}
 end
-
-#for op in (:(Base.getindex),:(Base.size),:(Base.length))
-#    @eval $op(S::MySumSpace...)=$op(S.spaces...)
-#end
+struct MatrixOperator{T} <: Operator{T}
+    matrix::Array{<:Operator{T},2}
+end
 
 Base.getindex(S::MySumSpace,k::Integer)=MySumSpace.spaces[k]
 Base.length(S::MySumSpace)=Base.length(S.spaces)
+Base.size(S::MySumSpace)=Base.size(S.spaces)
+Base.getindex(D::MatrixOperator,k::Integer,m::Integer)=D.matrix[k,m]
+Base.getindex(D::MatrixOperator,k::Integer)=D.matrix[k]
+Base.size(D::MatrixOperator)=size(D.matrix)
 
 evaluate(f::AbstractArray{<:AbstractArray,1}, S::MySumSpace, x) = sum(evaluate.(f,S.spaces,x))
 
@@ -17,14 +20,3 @@ function Conversion(S1::MySumSpace, S2::MySumSpace)
     @assert length(S1) == length(S2)
     MatrixOperator([m==n ? Conversion(S1[m],S2[n]) : ZeroOperator(S1[m],S2[n]) for n in 1:length(S2), m in 1:length(S1)])
 end
-    
-struct MatrixOperator{T} <: Operator{T}
-    matrix::Array{<:Operator{T},2}
-end
-
-#for op in (:(Base.getindex),:(Base.size),:*)
-#    @eval $op(D::MatrixOperator...)=$op(D.matrix...)
-#end
-Base.getindex(D::MatrixOperator,k::Integer,m::Integer)=D.matrix[k,m]
-Base.getindex(D::MatrixOperator,k::Integer)=D.matrix[k]
-Base.size(D::MatrixOperator)=size(D.matrix)
