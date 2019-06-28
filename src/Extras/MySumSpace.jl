001ef66cd27515ce0f6df634704269dbed4473a0
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2019-06-28 14:33:11 +0100

    Update MySumSpace.jl

--- src/Extras/MySumSpace.jl
+++ src/Extras/MySumSpace.jl
@@ -1,15 +1,15 @@
 export MySumSpace,MatrixOperator
 
-mutable struct MySumSpace{D,R} <: Space{D,R}
+struct MySumSpace{D,R} <: Space{D,R}
     spaces::Array{<:Space{D,R},1}
 end
 
-for op in (:(Base.getindex),:(Base.size),:(Base.length))
-    @eval $op(S::MySumSpace...)=$op(S.spaces...)
-end
+#for op in (:(Base.getindex),:(Base.size),:(Base.length))
+#    @eval $op(S::MySumSpace...)=$op(S.spaces...)
+#end
 
-#Base.getindex(S::MySumSpace,k::Integer)=MySumSpace.spaces[k]
-#length(S::MySumSpace)=length(S.spaces)
+Base.getindex(S::MySumSpace,k::Integer)=MySumSpace.spaces[k]
+Base.length(S::MySumSpace)=Base.length(S.spaces)
 
 evaluate(f::AbstractArray{<:AbstractArray,1}, S::MySumSpace, x) = sum(evaluate.(f,S.spaces,x))
 
@@ -22,9 +22,9 @@ struct MatrixOperator{T} <: Operator{T}
     matrix::Array{<:Operator{T},2}
 end
 
-for op in (:(Base.getindex),:(Base.size),:*)
-    @eval $op(D::MatrixOperator...)=$op(D.matrix...)
-end
-#Base.getindex(D::MatrixOperator,k::Integer,m::Integer)=D.matrix[k,m]
-#Base.getindex(D::MatrixOperator,k::Integer)=D.matrix[k]
-#Base.size(D::MatrixOperator)=size(D.matrix)
+#for op in (:(Base.getindex),:(Base.size),:*)
+#    @eval $op(D::MatrixOperator...)=$op(D.matrix...)
+#end
+Base.getindex(D::MatrixOperator,k::Integer,m::Integer)=D.matrix[k,m]
+Base.getindex(D::MatrixOperator,k::Integer)=D.matrix[k]
+Base.size(D::MatrixOperator)=size(D.matrix)
