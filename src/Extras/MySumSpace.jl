9d91234e84697788867de982387c129a2a2e5bd8
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2019-06-28 21:54:32 +0100

    Update MySumSpace.jl

--- src/Extras/MySumSpace.jl
+++ src/Extras/MySumSpace.jl
@@ -5,9 +5,6 @@ struct MySumSpace{D,R} <: Space{D,R}
 end
 struct MatrixOperator{T} <: Operator{T}
     matrix::Array{<:Operator{T},2}
-    domainspace::MySumSpace
-    rangespace::MySumSpace
-    # domainspace and rangspace can be obtained from matrix, a validity check is omitted here. Be careful to manually set the spaces.
 end
 
 # Basic
@@ -18,10 +15,8 @@ Base.getindex(D::MatrixOperator,k::Integer,m::Integer)=D.matrix[k,m]
 Base.getindex(D::MatrixOperator,k::Integer)=D.matrix[k]
 Base.size(D::MatrixOperator)=size(D.matrix)
 
-domainspace(matrix::Array{<:Operator,2})=MySumSpace([promote_type(domainspace.(matrix[:,n])...) for n in 1:size(matrix)[2]])
-rangespace(matrix::Array{<:Operator,2})=MySumSpace([promote_type(rangespace.(matrix[m,:])...) for m in 1:size(matrix)[1]])
-
-MatrixOperator(matrix::Array{<:Operator,2})=MatrixOperator(matrix,domainspace(matrix),rangespace(matrix))
+domainspace(S::MatrixOperator)=MySumSpace([promote_type(domainspace.(S.matrix[:,n])...) for n in 1:size(S.matrix)[2]])
+rangespace(S::MatrixOperator)=MySumSpace([promote_type(rangespace.(S.matrix[m,:])...) for m in 1:size(S.matrix)[1]])
 
 *(D::MatrixOperator,f::Array{<:AbstractArray,1})=D.matrix*f
 
@@ -29,10 +24,10 @@ evaluate(f::AbstractArray{<:AbstractArray,1}, S::MySumSpace, x) = sum(evaluate.(
 
 function Conversion(S1::MySumSpace, S2::MySumSpace)
     @assert length(S1) == length(S2)
-    MatrixOperator([m==n ? Conversion(S1[m],S2[n]) : ZeroOperator(S1[m],S2[n]) for n in 1:length(S2), m in 1:length(S1)], S1, S2)
+    MatrixOperator([m==n ? Conversion(S1[m],S2[n]) : ZeroOperator(S1[m],S2[n]) for n in 1:length(S2), m in 1:length(S1)])
 end
 
 function Conversion(S1::MySumSpace, S2::MySumSpace, plan::Array{<:Integer,1})
     @assert length(S1) == length(S2)
-    MatrixOperator([plan[m]==n ? Conversion(S1[m],S2[n]) : ZeroOperator(S1[m],S2[n]) for n in 1:length(S2), m in 1:length(S1)], S1, S2)
+    MatrixOperator([plan[m]==n ? Conversion(S1[m],S2[n]) : ZeroOperator(S1[m],S2[n]) for n in 1:length(S2), m in 1:length(S1)])
 end
