3f663c574fd4eae3416fd6760f87a6eb262e9e3a
Tianyi Pu <44583944+putianyi889@users.noreply.github.com>
2019-06-29 22:56:56 +0100

    Revert "Pty/sumspace"

--- src/Extras/MySumSpace.jl
+++ src/Extras/MySumSpace.jl
@@ -44,20 +44,18 @@ spacescompatible(S1::MySumSpace,S2::MySumSpace)=all(spacescompatible.(S1.spaces,
 
 # Interact with coefficients
 *(D::MatrixOperator,f::Array{<:AbstractArray,1})=D.matrix*f
+
 evaluate(f::AbstractArray{<:AbstractArray,1}, S::MySumSpace, x) = sum(evaluate.(f,S.spaces,x))
 
-# MatrixOperator Constructors
+# MatrixOperator Constructor
 function Conversion(S1::MySumSpace, S2::MySumSpace)
     @assert length(S1) == length(S2)
     MatrixOperator([m==n ? Conversion(S1[m],S2[n]) : ZeroOperator(S1[m],S2[n]) for n in 1:length(S2), m in 1:length(S1)])
 end
 
-# Derivative
-Derivative(S::MySumSpace,k::Integer)=MatrixOperator(
-    [m==n ? 
-        Derivative(S[m],k) : 
-        ZeroOperator(
-            S[m],rangespace(Derivative(S[n],k))) 
-        for 
-            n in 1:length(S), 
-            m in 1:length(S)])
+function Conversion(S1::MySumSpace, S2::MySumSpace, plan::Array{<:Integer,1})
+    @assert length(S1) == length(S2)
+    MatrixOperator([plan[m]==n ? Conversion(S1[m],S2[n]) : ZeroOperator(S1[m],S2[n]) for n in 1:length(S2), m in 1:length(S1)])
+end
+
+Derivative(S::MySumSpace,k::Integer)=MatrixOperator([m==n ? Derivative(S[m],k) : ZeroOperator(S[m],rangespace(Derivative(S[n],k))) for n in 1:length(S), m in 1:length(S)])
